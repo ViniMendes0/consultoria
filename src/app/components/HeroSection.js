c68489cd901be7a14586cf1a007b60d63283ce27
Vinicius Mendes <mendesvv96@gmail.com>
2025-03-01 11:16:14 -0300

    resp video e txt

--- src/app/components/HeroSection.js
+++ src/app/components/HeroSection.js
@@ -1,13 +1,29 @@
 const HeroSection = () => {
   return (
-    <div className="relative w-full flex flex-col items-center justify-center -mt-96 -top-28 text-center ">
-      <p className="text-4xl font-extrabold">
-        <em className="font-poppins text-[#344A6F] text-opacity-80 ">
-          CONSULTORIA PARA NEGÓCIOS.
-        </em>
-      </p>
-      <div className="mt-6">
-        <h1 className="text-2xl font-medium text-[#000000] text-opacity-55">
+    <div className="relative w-full flex flex-col items-center justify-center -mt-96 -top-28 text-center z-10 px-4 sm:px-0">
+      {/* Versão para TELAS GRANDES */}
+      <div className="hidden sm:block">
+        <p className="text-4xl font-extrabold">
+          <em className="font-poppins text-[#344A6F] text-opacity-80">
+            CONSULTORIA PARA NEGÓCIOS.
+          </em>
+        </p>
+        <div className="mt-6">
+          <h1 className="text-2xl font-medium text-black text-opacity-55">
+            Sua recolocação no mercado começa aqui!
+          </h1>
+        </div>
+      </div>
+
+      {/* Versão para TELAS MENORES */}
+      <div className="block sm:hidden">
+        <p className="text-xl font-extrabold  mt-44">
+          <em className="font-poppins text-[#344A6F] text-opacity-80">
+            CONSULTORIA PARA NEGÓCIOS.
+          </em>
+        </p>
+
+        <h1 className="text-lg font-medium text-black text-opacity-55 ">
           Sua recolocação no mercado começa aqui!
         </h1>
       </div>
--- src/app/components/VideoBackground.js
+++ src/app/components/VideoBackground.js
@@ -1,10 +1,11 @@
 const VideoBackground = () => {
   return (
-    <div className="-top-20 relative w-full h-screen overflow-hidden ">
+    <div className="-top-20 relative w-full min-h-screen sm:min-h-[500px] md:h-screen overflow-hidden">
       <video
         autoPlay
         muted
         loop
+        playsInline
         className="absolute top-0 left-0 w-full h-full object-cover opacity-80"
       >
         <source src="/videoc.mp4" type="video/mp4" />
